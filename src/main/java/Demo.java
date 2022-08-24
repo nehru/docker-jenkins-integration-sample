a205681876773fdc7c742fdeee3899ff9a99982a
Nehru <nehrusathappan@gmail.com>
2022-08-24 10:13:41 -0700

    refactor

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("inside docker jenkins");
-        System.out.println("version");
+        System.out.println("version 67");
     }
 }
